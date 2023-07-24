ec5fb2ed2a93ee754ae77b449eb507a6657c8756
Clay <clayshaw@courseratest.com>
2023-07-24 08:36:46 +0200

    restructured project

---
+++ src/FirstProject.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+/**
+ *
+ * @author DoD_Admin
+ */
+public class FirstProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        ReportMods reportMods = new ReportMods();
+        
+    ArrayList<Report> ReportList = reportMods.createArrayList();
+             
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please provide Report name:  ");
+    String ReportSearch = scanner.nextLine();
+    Report ReporttoSearch = reportMods.SearchbyName(ReportList, ReportSearch);
+        if (ReporttoSearch != null) {
+            System.out.println("Here is your requested report:   " + ReporttoSearch);
+        }
+     //Scanner scanner = new Scanner(System.in);
+        System.out.println("Would you like to enter a report?  ");
+        
+    Report ReportToAdd =  reportMods.AddReport(ReportList, ReportSearch, 0, 0, 0);
+        System.out.println("Name?  ");
+        String Name = scanner.nextLine();
+        System.out.println("ID?  ");
+        int ID = scanner.nextInt();
+        System.out.println("X cord?  ");
+        double Xcord = scanner.nextInt();
+        System.out.println("Y cord?   ");
+        double Ycord = scanner.nextInt();
+        
+        ReportList.add(ReportToAdd);
+        
+        
+        for (Report Reports : ReportList) {
+            System.out.println(Reports);
+        }
+        
+        
+        //System.out.println(ReportList );
+       // String ReportAdd = scanner.nextLine();
+       // String ReportAdd = reportMods.AddReport( String name, int ID, double Xcord, double Ycord);
+       // System.out.println("What is the Report Name");
+       // String name = scanner.nextLine();
+       // int ID = scanner.nextLine();
+      //  Double Xcord = scanner.nextLine();
+       // Double Ycord = scanner.nextLine();       
+                
+       // reportMods.AddReport(ID, name, 0, 0);
+        //for (Report Reports : ReportList) {
+       // System.out.println("Name: " + Reports.getName());
+       // System.out.println("ID: " + Reports.getID());
+       // System.out.println("X cord: " + Reports.getXcord());
+       // System.out.println("Y cord: " + Reports.getYcord());   
+      //  }
+      
+     
+     
+    }
+
+}
+
+
---
+++ src/Report.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author DoD_Admin
+ */
+public class Report {
+    //instance variables 
+    
+    private String name;
+    private int ID; 
+    private Double Xcord;
+    private Double Ycord;
+    
+Report(String name, int ID, Double Xcord, Double Ycord) {
+this.name = name;
+this.ID = ID;
+this.Xcord = Xcord;
+this.Ycord = Ycord;
+}
+    
+
+ public String getName() {
+     return name; 
+ }
+ 
+
+
+ public int getID() {
+     return ID;
+ }
+ 
+
+ 
+ public Double getXcord() {
+     return Xcord; 
+ }
+ 
+
+ public Double getYcord() {
+     return Ycord; 
+ }
+ 
+ 
+    @Override
+    public String toString() {
+    return "Name: " + name + " ID: " + ID + " XCord: " + Xcord + " YCord: " + Ycord; 
+         
+} 
+}
+
+
